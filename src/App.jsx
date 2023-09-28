eabd502ad886050b2f14764aa1e8613e077ae09f
dev-sg <sgbro33@gmail.com>
2023-09-28 23:00:24 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import Plx from "react-plx";
 import React, { useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
+import { Link } from "react-router-dom";
+import { AiOutlineDown } from "react-icons/ai";
+import { useRef } from "react";
 
 export default function App() {
-const [showScrollText, setShowScrollText] = useState(true);
-const [showPlayButton, setShowPlayButton] = useState(false);
+  const [showScrollText, setShowScrollText] = useState(true);
+  const [showPlayButton, setShowPlayButton] = useState(false);
+  const [showPressEnter, setShowPressEnter] = useState(false);
+  const scrollRef = useRef(null);
 
   useEffect(() => {
-   const handleScroll = () => {
+    const handleScroll = () => {
       const scrollY = window.scrollY;
 
       if (scrollY > 100) {
@@ -16,32 +22,72 @@ const [showPlayButton, setShowPlayButton] = useState(false);
         setShowScrollText(true);
       }
 
-      if (scrollY >= document.documentElement.scrollHeight - window.innerHeight) {
+      if (
+        scrollY >=
+        document.documentElement.scrollHeight - window.innerHeight
+      ) {
         setShowPlayButton(true);
       } else {
         setShowPlayButton(false);
       }
     };
 
+    const handleKeyPress = (event) => {
+      if (event.key === "Enter") {
+        smoothScrollTo(scrollRef.current);
+      }
+    };
+
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("keydown", handleKeyPress);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("keydown", handleKeyPress);
     };
   }, []);
+
+  const smoothScrollTo = (element) => {
+    const targetY = element.getBoundingClientRect().top + window.scrollY;
+    const initialY = window.scrollY;
+    const distance = Math.abs(targetY - initialY);
+    const duration = 5000; // Adjust the duration (in milliseconds) for slower or faster scrolling
+
+    let start;
+
+    const step = (timestamp) => {
+      if (!start) start = timestamp;
+
+      const time = timestamp - start;
+      const percent = Math.min(time / duration, 1);
+
+      window.scrollTo(0, initialY + percent * (targetY - initialY));
+
+      if (time < duration) {
+        requestAnimationFrame(step);
+      }
+    };
+
+    requestAnimationFrame(step);
+  };
+
+
+
   return (
+
+
     <div>
 
       <Plx
         parallaxData={[
           {
-            start: 500,
-            end: 600,
+            start: 600,
+            end: 700,
             easing: "ease-in",
             properties: [
               {
                 startValue: 0,
-                endValue: 55,
+                endValue: 45,
                 property: "translateY"
               }
             ]
@@ -49,18 +95,17 @@ const [showPlayButton, setShowPlayButton] = useState(false);
         ]}
         style={{
           position: "fixed",
-          left: "30vw",
+          left: "50%",
           top: "-50px",
-          width: "30%",
+          width: "30vw",
           zIndex: 200
         }}
       >
-        <nav style={{paddingTop: "5px", width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between", color: "white",gap:"110px"}}>
+        <nav className="home-nav" style={{paddingTop:"-20px", display: "flex",height: "32px", paddingBottom: "5px", transform: "translateX(-50%)", flexDirection: "row", justifyContent: "space-evenly", alignItems: "flex-end", color: "white"}}>
           <a style={{textDecoration: "none", color:"white"}}>Home</a>
           <a style={{textDecoration: "none", color:"white"}} >About</a>
           <a style={{textDecoration: "none", color:"white"}} >Project</a>
           <a style={{textDecoration: "none", color:"white"}} >Developers</a>
-          
         </nav>
       </Plx>
 
@@ -222,6 +267,11 @@ const [showPlayButton, setShowPlayButton] = useState(false);
                 startValue: 0,
                 endValue: 0,
                 property: "opacity",
+              },
+              {
+                startValue: 1,
+                endValue: 0.9,
+                property: "brightness",
               }
             ]
           },
@@ -264,24 +314,44 @@ const [showPlayButton, setShowPlayButton] = useState(false);
           src="/background.jpg"
         />
       </Plx>
-{showScrollText && (
+
+
+
+
+
+
+
+
+
+
+      {showScrollText && (
         <div
-        className={`scroll-down-container ${showScrollText ? "scroll-visible" : ""}`}
-        style={{
-          position: "fixed",
-          bottom: "10px", // Adjust the bottom position
-          left: "50%",
-          transform: "translateX(-50%)",
-          zIndex: 1000,
-        }}
-      >
-        <span className="scroll-text">Scroll</span>
-        <span className="arrow">↓</span>
+          className={`scroll-down-container ${
+            showScrollText ? "scroll-visible" : ""
+          }`}
+          style={{
+            position: "fixed",
+            bottom: "10px", // Adjust the bottom position
+            left: "50%",
+            transform: "translateX(-50%)",
+            zIndex: 1000,
+          }}
+        >
+          <span className="scroll-text arrow">Scroll</span>
+          <AiOutlineDown className="arrow" />
+          <span className="press-enter-text">
+            or Press Enter
+            {showPressEnter && (
+              <span className="press-enter-highlight"> (Press Enter)</span>
+            )}
+          </span>
         </div>
       )}
       {showPlayButton && (
         <div
-        className={`play-button-container ${showPlayButton ? "crash" : ""}`}
+          className={`play-button-container ${
+            showPlayButton ? "crash" : ""
+          }`}
           style={{
             position: "fixed",
             top: "70%",
@@ -289,15 +359,15 @@ const [showPlayButton, setShowPlayButton] = useState(false);
             transform: "translate(-50%, -50%)",
             zIndex: 1000,
           }}
-        > 
-       <button style={{ background: "transparent",color:"green" }}>
-  <h1>
-    <span>PLAY</span>
-  </h1>
-</button>
-        
+        >
+          <button style={{ background: "transparent", color: "green" }}>
+            <h1>
+              <span>PLAY</span>
+            </h1>
+          </button>
         </div>
       )}
+      <div ref={scrollRef}></div>
     </div>
   );
 }
