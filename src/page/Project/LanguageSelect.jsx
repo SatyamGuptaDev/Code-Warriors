f51002ceeaba09f0533f98bdca689a33dfc7a084
Satyam0327 <satyamsingh0327@gmail.com>
2023-11-18 15:24:15 +0530

    deone

--- src/page/Project/LanguageSelect.jsx
+++ src/page/Project/LanguageSelect.jsx
@@ -49,7 +49,9 @@ function LanguageSelect() {
 
                 }, 2000)
             } */}
-            <img id='minion' src='namaste_minion.png'  />
+              <div id="image-container">
+                       <img src="./Untitled-video-Made-with-Clipc-unscreen.gif" alt="Dancing Jimmy Fallon" />
+                  </div>
         </div>
         </>
     )
